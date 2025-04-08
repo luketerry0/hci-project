7fcd348d8f29582275acdbbbb1b570891a8bdd8c
Luke Terry <luke.terry0682@gmail.com>
2025-04-08 18:55:57 -0500

    remove flip display from currency

--- hci-project/src/app/counter/counter.component.ts
+++ hci-project/src/app/counter/counter.component.ts
@@ -1,10 +1,7 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
-import Tick from '@pqina/flip';
-import '@pqina/flip/dist/flip.min.css';
 import { GameStateService } from '../services/game-state.service';
 import { Subscription } from 'rxjs';
 
-declare var tick: any; // declare the global Tick object from flip.min.js
 
 @Component({
   selector: 'counter',
@@ -14,7 +11,7 @@ declare var tick: any; // declare the global Tick object from flip.min.js
   styleUrl: './counter.component.css'
 
 })
-export class CounterComponent implements AfterViewInit {
+export class CounterComponent {
   subscription: Subscription;
   currBalance: number = 0;
   tickInstance: any;
@@ -22,21 +19,7 @@ export class CounterComponent implements AfterViewInit {
 
     this.subscription = gss.currBalance$.subscribe((currBalance: number) => {
       this.currBalance = currBalance;
-      if (this.tickInstance){
-        this.tickInstance.value = this.currBalance;
-      }
     });
   }
 
-  @ViewChild('tickElement', { static: true }) tickElement!: ElementRef;
-
-  ngAfterViewInit(): void {
-    if (typeof Tick === 'undefined') {
-      console.error('Tick library is not loaded.');
-      return;
-    }
-
-    this.tickInstance = Tick.DOM.create(this.tickElement.nativeElement);
-    this.tickInstance.value = this.currBalance;
-  }
 }
